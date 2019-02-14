14902a9613bd8950157c3b94108cb59aa62c87ba
ben-solomon <bsbs93@gmail.com>
2019-02-13 22:09:28 -0600

    V2

--- LAB1/functions.js
+++ LAB1/functions.js
@@ -1,5 +1,4 @@
-s
-
+var curCategory = "";
 function sideNavToggle() {
     if (document.getElementById('sideNav').style.width == "200px"){
         document.getElementById('subNav').style.transition = "0.0s";
@@ -29,36 +28,82 @@ function subNavShow() {
             //document.getElementById('mainContent').style.marginLeft = "22%";
         }
 }
+
+function clearNav(){
+    document.getElementById('subNav').style.transition = "0.0s";
+    document.getElementById('sideNav').style.transition = "0.1s";
+    document.getElementById('sideNav').style.width = "0";
+    document.getElementById('subNav').style.width = "0";
+
+}
 function numToRating(i){
    return `&#8902;`.repeat(i);
 }
-function loadCourses(){
+function loadCourses(category){
     var HTMLstring = "";
-    var template = "";
     for (i=0;i<CourseDB.length;i++){
-        HTMLstring = `<div class='cardcontainer'>
-                                <div class='leftcard'>
-                                        <img src='img/${CourseDB[i].pic}'>
-                                </div>
-                                <div class = 'middlecard'>
-                                    <div class='flexcolumn'>
-                                        <h2>${CourseDB[i].name}</h2>
-                                        <div class='flexrow'>
-                                            <div class='middlecard'>
-                                                <p>${CourseDB[i].date}</p>
-                                                <p>${CourseDB[i].instructor}</p>
-                                                <p>${CourseDB[i].hours} Hours / ${CourseDB[i].lectures} Lectures </p>
-                                            </div> 
-                                            <div class='rightcard'>
-                                                <h2>${CourseDB[i].price}</h2>
-                                                <p class='ratingstars'>${numToRating(CourseDB[i].Rating)}</p>
-                                                <p><button class='addtocart'>Enroll</button></p>
-                                                
-                                            </div>
-                                        </div>
-                                    </div>
-                            </div></div>` + HTMLstring
+        if (CourseDB[i].Category == category || category ==""){
+            HTMLstring = `<div class='cardcontainer'>
+            <div class='leftcard'>
+                    <img src='img/${CourseDB[i].pic}'>
+            </div>
+            <div class = 'middlecard'>
+                <div class='flexcolumn'>
+                    <h2>${CourseDB[i].name}</h2>
+                    <div class='flexrow'>
+                        <div class='middlecard'>
+                            <p><strong>Created:</strong> ${CourseDB[i].date}</p>
+                            <p><strong>Instructor:</strong> ${CourseDB[i].instructor}</p>
+                            <p>${CourseDB[i].hours} Hours / ${CourseDB[i].lectures} Lectures </p>
+                        </div> 
+                        <div class='rightcard'>
+                            <h2>${CourseDB[i].price}</h2>
+                            <p class='ratingstars'>${numToRating(CourseDB[i].Rating)}</p>
+                            <p><button class='addtocart'>Enroll</button></p>
+                            
+                        </div>
+                    </div>
+                </div>
+        </div></div>` + HTMLstring
+}
+document.getElementById('mainContent').innerHTML = HTMLstring;
+        }
+        
     }
-    document.getElementById('mainContent').innerHTML = HTMLstring;
+function searchCourses(textevent){
+    var HTMLstring = "";
+    var str = textevent.value.toString().toLowerCase();
+    if (str == ""){
+        loadCourses("");
+        return;
     }
-    
+    if (str.length > 1){
+        for (i=0;i<CourseDB.length;i++){
+            if (CourseDB[i].name.toLowerCase().indexOf(str) >=0 ||CourseDB[i].Category.toLowerCase().indexOf(str) >= 0 ){
+                HTMLstring = `<div class='cardcontainer'>
+                <div class='leftcard'>
+                        <img src='img/${CourseDB[i].pic}'>
+                </div>
+                <div class = 'middlecard'>
+                    <div class='flexcolumn'>
+                        <h2>${CourseDB[i].name}</h2>
+                        <div class='flexrow'>
+                            <div class='middlecard'>
+                                <p>Created: ${CourseDB[i].date}</p>
+                                <p>Instructor: ${CourseDB[i].instructor}</p>
+                                <p>${CourseDB[i].hours} Hours / ${CourseDB[i].lectures} Lectures </p>
+                            </div> 
+                            <div class='rightcard'>
+                                <h2>${CourseDB[i].price}</h2>
+                                <p class='ratingstars'>${numToRating(CourseDB[i].Rating)}</p>
+                                <p><button class='addtocart'>Enroll</button></p>
+                                
+                            </div>
+                        </div>
+                    </div>
+            </div></div>` + HTMLstring
+            }
+            document.getElementById('mainContent').innerHTML = HTMLstring;
+        }
+    }
+}
