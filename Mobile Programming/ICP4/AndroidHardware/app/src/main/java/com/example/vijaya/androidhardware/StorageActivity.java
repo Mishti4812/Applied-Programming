a40e98b989529a231405260f420cc088cd742498
Vrusha Patel <vrushapatel4812@gmail.com>
2019-04-20 16:01:16 -0500

    ICP4

---
+++ Mobile Programming/ICP4/AndroidHardware/app/src/main/java/com/example/vijaya/androidhardware/StorageActivity.java
@@ -0,0 +1,93 @@
+package com.example.vijaya.androidhardware;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class StorageActivity extends AppCompatActivity {
+    EditText txt_content;
+    EditText contenttoDisplay;
+    String FILENAME = "MyAppStorage";
+    Button save;
+    Button displaySavedText;
+    static final String errorControl = "error";
+    String retrieveData;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_storage);
+        txt_content = (EditText) findViewById(R.id.id_txt_mycontent);
+        contenttoDisplay = (EditText) findViewById(R.id.id_txt_display);
+        save = (Button) findViewById(R.id.id_btn_save);
+        displaySavedText = (Button) findViewById(R.id.id_btn_display);
+    }
+
+
+    public void saveTofile(View v) throws IOException {
+
+        // ICP Task4: Write the code to save the text
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String text=txt_content.getText().toString();
+                FileOutputStream fileOutput=null;
+                try{
+                    fileOutput=openFileOutput(FILENAME, getApplicationContext().MODE_APPEND);
+                    fileOutput.write( text.getBytes());
+                    fileOutput.close();
+                }catch(Exception e)
+                {
+                    Log.d(errorControl,"unable to save file");
+
+                }
+            }
+        });
+
+    }
+
+    public void retrieveFromFile(View v) throws IOException {
+
+        // ICP Task4: Write the code to display the above saved text
+        displaySavedText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FileInputStream fileInput =null;
+                try {
+                    fileInput = openFileInput(FILENAME);
+                    InputStreamReader inputStreamReader = new InputStreamReader(fileInput);
+                    BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                    String receiveString = "";
+                    StringBuilder stringBuilder = new StringBuilder();
+                    while ((receiveString = bufferedReader.readLine())!= null){
+                        stringBuilder.append(receiveString);
+                    }
+                    fileInput.close();
+                    retrieveData = stringBuilder.toString();
+                    contenttoDisplay.setText(retrieveData);
+                    contenttoDisplay.setVisibility(View.VISIBLE);
+
+
+                    contenttoDisplay.setText(txt_content.getText());
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+}
